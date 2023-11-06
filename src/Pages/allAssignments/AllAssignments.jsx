df4183c3905e9c3c37e94a65ad437c97395a6a86
rashednyn <rashedmahamud22@gmail.com>
2023-11-06 14:31:52 +0600

    reset all assignments after delete

--- src/Pages/allAssignments/AllAssignments.jsx
+++ src/Pages/allAssignments/AllAssignments.jsx
@@ -1,21 +1,70 @@
 import { useLoaderData } from "react-router-dom";
 import AssignmentCard from "./AssignmentCard";
-
-
+import Swal from "sweetalert2";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../providers/AuthProviders";
 
 const allAssignments = () => {
-    const assignments = useLoaderData();
+  const allAssignments = useLoaderData();
+  const { user } = useContext(AuthContext);
 
-    return (
-        <div>
-            <h2 className="text-3xl text-center">All Assignment: {assignments.length}</h2>
-            <div>
-                {
-                    assignments.map(assignment=><AssignmentCard key={assignment._id} assignment={assignment}></AssignmentCard>)
+  const [assignments, setAssignments] = useState(allAssignments);
+
+  const handleDelete = (_id, creatorEmail) => {
+    if (user) {
+      if (user?.email !== creatorEmail) {
+        console.log("you are not authorized");
+      } else {
+        console.log("Delete:", _id);
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            fetch(`http://localhost:5000/assignments/${_id}`, {
+              method: "DELETE",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                if (data.deletedCount > 0) {
+                  console.log("deleted successfully");
+                  Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success",
+                  });
+                  const remainingAssignment = assignments.filter(assignment=>assignment._id !==_id)
+                  setAssignments(remainingAssignment);
                 }
-            </div>
-        </div>
-    );
+              });
+          }
+        });
+      }
+    } else {
+      console.log("Please Login First");
+    }
+  };
+  return (
+    <div>
+      <h2 className="text-3xl text-center">
+        All Assignment: {assignments.length}
+      </h2>
+      <div>
+        {assignments.map((assignment) => (
+          <AssignmentCard
+            key={assignment._id}
+            assignment={assignment}
+            handleDelete={handleDelete}
+          ></AssignmentCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default allAssignments;
--- src/Pages/allAssignments/AssignmentCard.jsx
+++ src/Pages/allAssignments/AssignmentCard.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProviders";
 import Swal from "sweetalert2";
+import { reload } from "firebase/auth";
 
-const AssignmentCard = ({ assignment }) => {
-    const {user}=useContext(AuthContext);
+const AssignmentCard = ({ assignment,handleDelete }) => {
+  const { user } = useContext(AuthContext);
+
   const {
     _id,
     title,
@@ -16,43 +18,42 @@ const AssignmentCard = ({ assignment }) => {
     creatorEmail,
   } = assignment;
 
-  const handleDelete=(_id)=>{
-    if(user){
-        if(user?.email!==creatorEmail){
-            console.log('you are not authorized');
-        }else{
-            // console.log('Delete:',_id);
-            Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!"
-              }).then((result) => {
-                if (result.isConfirmed) {
-                    fetch(`http://localhost:5000/assignments/${_id}`,{
-                        method:"DELETE",
-                    })
-                    .then(res=>res.json())
-                    .then(data=>{
-                        console.log('deleted successfully');
-                    })
-                //   Swal.fire({
-                //     title: "Deleted!",
-                //     text: "Your file has been deleted.",
-                //     icon: "success"
-                //   });
-                }
-              });
-        }
-    }else{
-        console.log('Please Login First');
-    }
+//   const handleDelete = (_id) => {
+//     if (user) {
+//       if (user?.email !== creatorEmail) {
+//         console.log("you are not authorized");
+//       } else {
+//         // console.log('Delete:',_id);
+//         Swal.fire({
+//           title: "Are you sure?",
+//           text: "You won't be able to revert this!",
+//           icon: "warning",
+//           showCancelButton: true,
+//           confirmButtonColor: "#3085d6",
+//           cancelButtonColor: "#d33",
+//           confirmButtonText: "Yes, delete it!",
+//         }).then((result) => {
+//           if (result.isConfirmed) {
+//             fetch(`http://localhost:5000/assignments/${_id}`, {
+//               method: "DELETE",
+//             })
+//               .then((res) => res.json())
+//               .then((data) => {
+//                 console.log("deleted successfully");
+//                 Swal.fire({
+//                   title: "Deleted!",
+//                   text: "Your file has been deleted.",
+//                   icon: "success",
+//                 });
+//               });
+//           }
+//         });
+//       }
+//     } else {
+//       console.log("Please Login First");
+//     }
+//   };
 
-    }
-    
   return (
     <div className="">
       <div className="card card-side bg-base-100 shadow-xl mb-4">
@@ -82,7 +83,7 @@ const AssignmentCard = ({ assignment }) => {
               Update
             </Link>
             <Link
-              onClick={() => handleDelete(_id)}
+              onClick={() => handleDelete(_id,creatorEmail)}
               className="btn btn-primary btn-sm"
             >
               Delete
